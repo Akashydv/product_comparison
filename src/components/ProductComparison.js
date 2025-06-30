0f4cd32466faa81e2b59945f548e1944156c380f
Akash Yadav <akii@Akashs-MacBook-Air.local>
2025-06-30 11:55:04 +0530

    rating

--- src/components/ProductComparison.js
+++ src/components/ProductComparison.js
@@ -7,17 +7,52 @@ import {
   TableRow,
   Paper,
   Typography,
+  Rating,
 } from "@mui/material";
 import ArrowBackIosSharpIcon from "@mui/icons-material/ArrowBackIosSharp";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { Button } from "@mui/material";
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function ProductComparisonTable({ productData, setCompare }) {
-  const comparedProducts = productData.filter((product) => product.selected);
+  const [comparedProducts, setComparedProducts] = useState([]);
+  useEffect(() => {
+    setComparedProducts(productData.filter((product) => product.selected));
+  }, []);
+
+  const removeProductFromComparison = (id) => {
+    setComparedProducts(comparedProducts.filter((product) => product.id != id));
+  };
+
   const featureKeys = Array.from(
     new Set(comparedProducts.flatMap((p) => Object.keys(p.keyFeatures)))
   );
-  debugger;
+
+  if (comparedProducts.length === 0) {
+    return (
+      <div className="col-12 p-3">
+        <Button
+          variant="outlined"
+          size="small"
+          className="mb-3 text-capitalize"
+          onClick={() => setCompare(false)}
+          startIcon={<ArrowBackIosSharpIcon />}
+        >
+          Back
+        </Button>
+        <div className="col-12 text-center">
+          <Typography
+            variant="captiontext"
+            p={2}
+            className="fst-italic fw-semibold"
+          >
+            You have removed all products from here. Kindly go back and come
+            again.
+          </Typography>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="col-12 p-3">
       <Button
@@ -42,6 +77,10 @@ export default function ProductComparisonTable({ productData, setCompare }) {
               {comparedProducts.map((p, idx) => (
                 <TableCell key={idx}>
                   <strong>{p.name}</strong>
+                  <DeleteOutlineIcon
+                    onClick={() => removeProductFromComparison(p.id)}
+                    style={{ marginLeft: "10px", color: "red" }}
+                  />
                 </TableCell>
               ))}
             </TableRow>
@@ -54,6 +93,22 @@ export default function ProductComparisonTable({ productData, setCompare }) {
               {comparedProducts.map((p, idx) => (
                 <TableCell key={idx}>
                   <img src={p.image} style={{ width: "100px" }} alt={p.name} />
+                </TableCell>
+              ))}
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <strong>Rating</strong>
+              </TableCell>
+              {comparedProducts.map((p, idx) => (
+                <TableCell key={idx}>
+                  <Rating
+                    name="read-only"
+                    value={p.rating}
+                    readOnly
+                    size="small"
+                    precision={0.5}
+                  />
                 </TableCell>
               ))}
             </TableRow>
